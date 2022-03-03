083d570c7749d69ef9a3008cd9c2d97f808f9291
decker213 <decker@htl-steyr.ac.at>
2022-03-03 13:57:00 +0100

    Person Klasse

---
+++ src/com/company/Person.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Person {
+    private String name;
+    private String givenname;
+    private Date birthday;
+    private Phone phonenumber;
+
+    public Person(String name, String givenname, Date birthday, Phone phonenumber) {
+        this.name = name;
+        this.givenname = givenname;
+        this.birthday = birthday;
+        this.phonenumber = phonenumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", givenname='" + givenname + '\'' +
+                ", birthday=" + birthday +
+                ", phonenumber=" + phonenumber +
+                '}';
+    }
+}

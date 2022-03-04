2d723e2623889bc9f18276e957902ac50598b798
decker213 <decker@htl-steyr.ac.at>
2022-03-04 09:21:51 +0100

    StreamOperation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
     }
 }
